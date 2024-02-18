9e41e1f0801b01b49a48eb6454f8190ccd8cdb77
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-18 23:27:59 +0600

    show user name, email and photo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 
 
 const Login = () => {
+
+     const [user, setUser] = useState(null);
+
      const auth = getAuth(app);
      const provider = new GoogleAuthProvider();
 
      const handleGoogleSignIn = () => {
           signInWithPopup(auth, provider)
                .then(result => {
-                    const user = result.user;
-                    console.log(user);
+                    const loggedUser = result.user;
+                    console.log(loggedUser);
+                    setUser(loggedUser);
                })
                .catch(error => {
                     console.log('error', error.message);
@@ -21,6 +25,14 @@ const Login = () => {
      return (
           <div>
                <button onClick={handleGoogleSignIn}>Google Login</button>
+               {
+                    user && 
+                    <div>
+                              <h2>User: {user.displayName}</h2>
+                              <p>Email: {user.email}</p>
+                              <img src={user.photoURL} alt="" />
+                    </div>
+               }
           </div>
      );
 };
